b98d8c785a3be8e0873d61a75e1ac069eaddc3e5
LuloDev <134250506+LuloDev@users.noreply.github.com>
2024-03-05 20:32:47 -0500

    Added Google Books integration
    The code now includes a new feature that fetches book data from Google Books API using ISBN. This information is then displayed to the user, including details such as title, authors, publication date, number of pages, language, publisher and description. The slash command 'Book Fetch' has been added to trigger this functionality.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,44 @@
 import '@logseq/libs';
+import { GoogleBookService } from './services/googleBookService';
+
+async function getDataBook(isbn: string): Promise<string[]> {
+  const bookService = new GoogleBookService();
+  const book = await bookService.getBook(isbn);
+  if (!book) {
+    logseq.UI.showMsg(`
+    [:div.p-2
+      [:h1 "Book not found!"]
+    ]
+    `);
+    return;
+  }
+  logseq.UI.showMsg(`
+  [:div.p-2
+    [:h1 "${book.title}"]
+  ]
+  `);
+  return [
+    `![${book.title}](${book.thumbnailUrl ?? ''})`,
+    `**Title:** ${book.title}`,
+    `**Authors:** [[${book.authors.join(', ')}]]`,
+    `**Published At:** ${book.publishedAt?.toLocaleDateString()}`,
+    `**Pages:** ${book.pages ?? 'N/A'}`,
+    `**Language:** ${book.language ?? 'N/A'}`,
+    `**Publisher:** ${book.publisher ?? 'N/A'}`,
+    `**Description:** ${book.description ?? 'N/A'}`,
+  ];
+}
 
 function main() {
-  logseq.Editor.registerSlashCommand('Big Bang', async () => {
+  logseq.Editor.registerSlashCommand('Book Fetch', async () => {
     const { content, uuid } = (await logseq.Editor.getCurrentBlock()) ?? {};
-
-    logseq.UI.showMsg(`
-        [:div.p-2
-          [:h1 "#${uuid}"]
-          [:h2.text-xl "${content}"]]
-      `);
+    if (!content) return;
+    const isbn = content;
+    const data = await getDataBook(isbn);
+    const blocks = data.map((it) => ({ content: it }));
+    await logseq.Editor.insertBatchBlock(uuid, blocks, {
+      sibling: false,
+    });
   });
 }
 logseq.ready(main).catch(console.error);
