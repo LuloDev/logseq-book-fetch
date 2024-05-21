5bc4f56e99739862316f252d589e5524f9afe05d
LuloDev <134250506+LuloDev@users.noreply.github.com>
2024-05-20 20:59:35 -0500

    Updated default book fetching template
    The book fetching template in the settings has been updated to include a thumbnail. This allows for an image of the book cover to be displayed alongside other details such as title, ISBN, authors, etc.

--- src/settings.ts
+++ src/settings.ts
@@ -4,10 +4,10 @@ export const settings: SettingSchemaDesc[] = [
   {
     key: 'template',
     description:
-      'The template to use when fetching a book. Use the following variables: title, isbn, authors, publishedAt, categories, pages, language, publisher, maturityRating, description',
+      'The template to use when fetching a book. Use the following variables: title, isbn, authors, publishedAt, categories, pages, language, publisher, maturityRating, description, thumbnail',
     type: 'string',
     default:
-      '- Title: {{title}}\n- ISBN: {{isbn}}\n- Authors: {{authors}}\n- Published At: {{publishedAt}}\n- Categories: {{categories}}\n- Pages: {{pages}}\n- Language: {{language}}\n- Publisher: {{publisher}}\n- Maturity Rating: {{maturityRating}}\n- Description: {{description}}',
+      '- ![Book Cover]({{thumbnail}})\n- Title: {{title}}\n- ISBN: {{isbn}}\n- Authors: {{authors}}\n- Published At: {{publishedAt}}\n- Categories: {{categories}}\n- Pages: {{pages}}\n- Language: {{language}}\n- Publisher: {{publisher}}\n- Maturity Rating: {{maturityRating}}\n- Description: {{description}}',
     title: 'Template',
   },
 ];
